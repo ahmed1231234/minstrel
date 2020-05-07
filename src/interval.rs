4bcd084b2eb37c3f9bce80581ee584100e46bdca
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-05-07 20:27:28 +1000

    Limit visibility of the `semitones` field for intervals

--- src/interval.rs
+++ src/interval.rs
@@ -6,7 +6,7 @@ pub const TONE: Interval = Interval::new(2);
 /// A musical interval, represented as a number of semitones.
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct Interval {
-    pub semitones: usize,
+    pub(crate) semitones: usize,
 }
 
 impl Interval {
