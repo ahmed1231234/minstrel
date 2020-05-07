cadf63fbdd9b3c24afa49b4c2e7102b92bfa7e7b
Thomas Freeman <tomclaudefreeman@gmail.com>
2020-05-07 14:52:08 +1000

    Fix visibility bug

--- src/interval.rs
+++ src/interval.rs
@@ -6,7 +6,7 @@ pub const TONE: Interval = Interval::new(2);
 /// A musical interval, represented as a number of semitones.
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 pub struct Interval {
-    semitones: usize,
+    pub semitones: usize,
 }
 
 impl Interval {
